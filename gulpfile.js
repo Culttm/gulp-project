55054e19d1607ab4b28af02365bd6bc5a905a66e
Zar <severenit@gmail.com>
2015-04-10 16:14:09 +0300

    Gulp Project

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  webserver = require('gulp-webserver'),
+  sass = require('gulp-sass'),
+  uncss = require('gulp-uncss'),
+  concat = require('gulp-concat'),
+  csso = require('gulp-csso');
+
+gulp.task('js', function() {
+  gulp.src('builds/development/js/**/*')
+});
+
+gulp.task('html', function() {
+  gulp.src('builds/development/*.html')
+});
+
+gulp.task('sass', function () {
+    gulp.src('builds/development/sass/*.scss')
+        .pipe(sass())
+        .pipe(concat('style.css'))
+        .pipe(uncss({
+            html: ['builds/development/*.html']
+        }))
+        .pipe(csso())
+        .pipe(gulp.dest('builds/development/css/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('builds/development/js/**/*', ['js']);
+  gulp.watch('builds/development/sass/*.scss', ['sass']);
+  gulp.watch(['builds/development/*.html',
+    'builds/development/views/*.html'], ['html']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src('builds/development/')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'sass', 'webserver']);
