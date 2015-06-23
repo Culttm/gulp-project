9725dcad81069e9863181235595be9b6ce3625ad
Zar <severenit@gmail.com>
2015-06-23 16:25:06 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,67 @@
+'use strict';
+
 var gulp = require('gulp'),
     webserver = require('gulp-webserver'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
-    csso = require('gulp-csso'),
-    gulpCopy = require('gulp-copy');
+    csso = require('gulp-csso');
+    
+var bc = './bower_components/';
 
 gulp.task('js', function() {
-  gulp.src('builds/development/js/**/*')
+  gulp.src('builds/development/js/**/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('builds/dist/app/'))
 });
 
 gulp.task('html', function() {
-  gulp.src('builds/development/*.html')
+  gulp.src('builds/development/**/*.html')
+    .pipe(gulp.dest('builds/dist/'))
 });
 
 gulp.task('sass', function () {
-    gulp.src('builds/development/sass/**/*')
-        .pipe(sass())
-        .pipe(concat('style.min.css'))
-        .pipe(csso())
-        .pipe(gulp.dest('builds/development/css/'));
+  gulp.src('builds/development/sass/**/*')
+      .pipe(sass())
+      .pipe(concat('style.min.css'))
+      .pipe(csso())
+      .pipe(gulp.dest('builds/dist/css/'));
+});
+
+gulp.task('img', function() {
+  gulp.src('builds/development/img/**/*')
+    .pipe(gulp.dest('builds/dist/img/'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('builds/development/js/**/*', ['js']);
-  gulp.watch('builds/development/sass/**/*', ['sass']);
-  gulp.watch(['builds/development/*.html',
-              'builds/development/views/*.html'], ['html']);
+  gulp.watch('builds/development/app/**/*.js', ['js']);
+  gulp.watch('builds/development/sass/**/*.scss', ['sass']);
+  gulp.watch('builds/development/**/*.html', ['html']);
+  gulp.watch('builds/development/img/**/*', ['img']);
+});
+
+gulp.task('libs', function() {
+  gulp.src(bc+'jquery/dist/jquery.js')
+      .pipe(gulp.dest('./builds/dist/libs/jquery/'));
+
+  gulp.src(bc+'bootstrap/dist/**/*.*')
+      .pipe(gulp.dest('./builds/dist/libs/bootstrap/'));
+
 });
 
 gulp.task('webserver', function() {
-  gulp.src('builds/development/')
-    .pipe(webserver({
-      livereload: true,
-      open: true
-    }));
+  gulp.src('builds/dist/')
+      .pipe(webserver({
+        livereload: true,
+        open: true
+      }));
 });
 
-gulp.task('copyfonts', function() {
-   gulp.src('./node_modules/bootstrap/fonts/**/*.{ttf,woff,woff2,eot,svg}')
-   .pipe(gulp.dest('./builds/development/fonts'));
-});
-
-gulp.task('copyjquery', function() {
-   gulp.src('./node_modules/jquery/dist/jquery.js')
-   .pipe(gulp.dest('./builds/development/js/lib/jquery/'));
-});
-
-gulp.task('copybootstrap', function() {
-   gulp.src('.//node_modules/bootstrap/dist/js/bootstrap.js')
-   .pipe(gulp.dest('./builds/development/js/lib/bootstrap/'));
-});
-
-
-gulp.task('default', ['copyfonts', 'copyjquery', 'copybootstrap', 'watch', 'html', 'js', 'sass', 'webserver']);
+gulp.task('default', [
+  'libs',
+  'html',
+  'img',
+  'js',
+  'sass',
+  'webserver',
+  'watch'
+]);
